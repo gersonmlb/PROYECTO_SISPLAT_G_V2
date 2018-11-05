9a593da99a2e3c1ce3eff581008914a2fe016d20
YaritzaC <yaritzacasildo@upeu.edu.pe>
2018-11-04 23:02:14 -0600

    prueba
    prueba

---
+++ springNomaven-1/WebContent/recursos/javascript/venta.js
@@ -0,0 +1,15 @@
+function prueba(){
+	swal({
+		  title: "Ajax request example",
+		  text: "Submit to run ajax request",
+		  type: "info",
+		  showCancelButton: true,
+		  closeOnConfirm: false,
+		  showLoaderOnConfirm: true
+		}, function () {
+		  setTimeout(function () {
+		    swal("Ajax request finished!");
+		  }, 2000);
+		});
+	
+}
