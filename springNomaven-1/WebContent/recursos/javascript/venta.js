5162f317cd42128d30e82ad6f2dd3be00634c5d3
YaritzaC <yaritzacasildo@upeu.edu.pe>
2018-11-07 23:40:40 -0600

    Venta, pedido
    venta pedido

--- springNomaven-1/WebContent/recursos/javascript/venta.js
+++ springNomaven-1/WebContent/recursos/javascript/venta.js
@@ -1,4 +1,13 @@
-
-function prueba(){
-	alert('You clicked the button!');
+function prueba() {
+	swal({
+		  position: 'top-end',
+		  type: 'success',
+		  title: 'Your work has been saved',
+		  showConfirmButton: false,
+		  timer: 1500
+		})
 }
+$("#prueba1").click(function() {
+	var num = $("#Contado").val();
+	alert(num);
+})
